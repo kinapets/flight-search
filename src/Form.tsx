a7e25edc3f6852cbc1fa5c1f9ff4605337066f5f
Radim Štěpaník <radim.stepanik@gmail.com>
2018-02-15 23:05:24 +0100

    Add autocompletion

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
-import {Form, DatePicker, Button} from 'antd';
+import {Form, DatePicker, Button, AutoComplete} from 'antd';
 import {FormComponentProps} from 'antd/lib/form';
+
 const FormItem = Form.Item;
 
-class TimeRelatedForm extends React.Component {
+class SearchForm extends React.Component {
+    state = {
+        results: []
+    }
+
     props: FormComponentProps;
     handleSubmit = (event: any) => {
         event.preventDefault();
@@ -15,11 +20,21 @@ class TimeRelatedForm extends React.Component {
 
             const values = {
                 ...fieldsValue,
-                'date-picker': new Date(fieldsValue['date-picker'].format('YYYY-MM-DD')),
+                'date-picker': new Date(fieldsValue['date'].format('YYYY-MM-DD')),
             };
             console.log(values);
         });
     }
+    handleSearch = (value: string) => {
+        let results: any;
+        if (!value || value.indexOf('@') >= 0) {
+          results = [];
+        } else {
+          results = ['gmail.com', '163.com', 'qq.com'].map(domain => `${value}@${domain}`);
+        }
+        this.setState({ results });
+      }
+
     render() {
         const {getFieldDecorator} = this.props.form;
         const formItemLayout = {
@@ -32,19 +47,39 @@ class TimeRelatedForm extends React.Component {
                 sm: {span: 16},
             },
         };
-        const config = {rules: [{type: 'object', required: true, message: 'Please select time!'}]};
+        const config = {rules: [{required: true, message: 'Please select time!'}]};
+        const configDate = {rules: [{type: 'object', required: true, message: 'Please select time!'}]};
 
         return (
             <Form onSubmit={this.handleSubmit}>
-                <FormItem {...formItemLayout} label="DatePicker">
-                    {getFieldDecorator('date-picker', config)(<DatePicker />)}
+                <FormItem {...formItemLayout} label="From">
+                    {getFieldDecorator('from', config)(
+                        <AutoComplete
+                            dataSource={this.state.results}
+                            style={{width: 171}}
+                            onSearch={this.handleSearch}
+                        />
+                    )}
+                </FormItem>
+                <FormItem {...formItemLayout} label="To">
+                    {getFieldDecorator('to', config)(
+                        <AutoComplete
+                            dataSource={this.state.results}
+                            style={{width: 171}}
+                            onSearch={this.handleSearch}
+                        />
+                    )}
                 </FormItem>
 
-                <FormItem wrapperCol={{xs: {span: 24, offset: 0},sm: {span: 16, offset: 8},}}>
+                <FormItem {...formItemLayout} label="Date">
+                    {getFieldDecorator('date', configDate)(<DatePicker />)}
+                </FormItem>
+
+                <FormItem wrapperCol={{xs: {span: 24, offset: 0}, sm: {span: 16, offset: 8}, }}>
                     <Button type="primary" htmlType="submit">Submit</Button>
                 </FormItem>
             </Form>
         );
     }
 }
-export default Form.create()(TimeRelatedForm);
+export default Form.create()(SearchForm);
