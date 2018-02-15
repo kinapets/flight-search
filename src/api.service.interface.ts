ce86e1397dfef75f0c0a303970d29b507dad6bc6
Radim Štěpaník <radim.stepanik@gmail.com>
2018-02-15 21:49:03 +0100

    style(api.service): Move interfaces to special file

---
+++ src/api.service.interface.ts
@@ -0,0 +1,94 @@
+
+export interface AbstractResponse {
+    statusCode: number;
+    errPayload?: any;
+}
+
+export interface HttpResponse<T> extends AbstractResponse {
+    data: T;
+}
+
+export interface GetFlights {
+    date: Date;
+    from: string;
+    to: string;
+    page: number;
+}
+
+export interface GetFlightsQueryParameters {
+    flyFrom?: string;
+    to?: string;
+    dateFrom?: string;
+    dateTo?: string;
+    sort?: string;
+    asc?: 1 | 0;
+    radiusFrom?: number;
+    curr?: string;
+    locale?: string;
+    offset?: number;
+    limit?: number;
+}
+
+export interface GetFlightsResponse {
+    [key: string]: any,
+    data: {
+        mapIdfrom: string;
+        duration: Object;
+        flyTo: string;
+        conversion: Object;
+        mapIdto: string;
+        airlines: any[];
+        id: string;
+        facilitated_booking_available: boolean;
+        pnr_count: number;
+        fly_duration: string;
+        countryTo: Object;
+        baglimit: Object;
+        aTimeUTC: number;
+        p3: number;
+        price: number;
+        bags_price: Object;
+        cityTo: string;
+        flyFrom: string;
+        dTimeUTC: number;
+        p2: number;
+        countryFrom: Object;
+        p1: number;
+        dTime: number;
+        found_on: any[];
+        booking_token: string;
+        routes: any[];
+        cityFrom: string;
+        aTime: number;
+        route: any[];
+        distance: number;
+    }
+}
+
+export interface GetLocationsResponse {
+    locations: {
+        id: string;
+        int_id: number;
+        active: boolean;
+        code: string;
+        name: string;
+        slug: string;
+        alternative_names: string[];
+        rank: number;
+        timezone: string;
+        city: {
+            id: string;
+            name: string;
+            code: string;
+            slug: string;
+            subdivision: any;
+            country: Object;
+            region: Object;
+            continent: Object;
+        };
+        location: {lat: number; lon: number};
+        alternative_departure_points: Object[];
+        type: string;
+    }[];
+    meta: any
+}
--- src/api.service.ts
+++ src/api.service.ts
@@ -1,103 +1,10 @@
 import {RxHR} from '@akanass/rx-http-request';
 import * as moment from 'moment';
 import {Observable} from 'rx';
+import {HttpResponse, GetFlightsQueryParameters, GetFlights, GetFlightsResponse, GetLocationsResponse} from './api.service.interface'
 
 const BASE_URL = 'https://api.skypicker.com';
 const RESULTS_ON_PAGE = 5;
-
-interface AbstractResponse {
-    statusCode: number;
-    errPayload?: any;
-}
-
-interface HttpResponse<T> extends AbstractResponse {
-    data: T;
-}
-
-interface GetFlights {
-    date: Date;
-    from: string;
-    to: string;
-    page: number;
-}
-
-interface GetFlightsQueryParameters {
-    flyFrom?: string;
-    to?: string;
-    dateFrom?: string;
-    dateTo?: string;
-    sort?: string;
-    asc?: 1 | 0;
-    radiusFrom?: number;
-    curr?: string;
-    locale?: string;
-    offset?: number;
-    limit?: number;
-}
-
-interface GetFlightsResponse {
-    [key: string]: any,
-    data: {
-        mapIdfrom: string;
-        duration: Object;
-        flyTo: string;
-        conversion: Object;
-        mapIdto: string;
-        airlines: any[];
-        id: string;
-        facilitated_booking_available: boolean;
-        pnr_count: number;
-        fly_duration: string;
-        countryTo: Object;
-        baglimit: Object;
-        aTimeUTC: number;
-        p3: number;
-        price: number;
-        bags_price: Object;
-        cityTo: string;
-        flyFrom: string;
-        dTimeUTC: number;
-        p2: number;
-        countryFrom: Object;
-        p1: number;
-        dTime: number;
-        found_on: any[];
-        booking_token: string;
-        routes: any[];
-        cityFrom: string;
-        aTime: number;
-        route: any[];
-        distance: number;
-    }
-}
-
-interface GetLocationsResponse {
-    locations: {
-        id: string;
-        int_id: number;
-        active: boolean;
-        code: string;
-        name: string;
-        slug: string;
-        alternative_names: string[];
-        rank: number;
-        timezone: string;
-        city: {
-            id: string;
-            name: string;
-            code: string;
-            slug: string;
-            subdivision: any;
-            country: Object;
-            region: Object;
-            continent: Object;
-        };
-        location: {lat: number; lon: number};
-        alternative_departure_points: Object[];
-        type: string;
-    }[];
-    meta: any
-}
 
 export function getFlights(flightParameters: GetFlights) {
     const {page, date, from, to} = flightParameters;
