649428d7eec32ec088cf1eaba52b17b97b0b2a28
Radim Štěpaník <radim.stepanik@gmail.com>
2018-02-15 21:47:17 +0100

    feat(api.service): Add get locations to API

--- src/api.service.test.ts
+++ src/api.service.test.ts
@@ -1,26 +1,39 @@
-import {getFlights} from './api.service';
+import {getFlights, getLocations} from './api.service';
 import * as should from 'should';
 
-describe('Api service test',() => {
-    it('should call api with success status code', (done) => {
-        getFlights({from: 'Paris', to: 'London', date: new Date('2018-02-13'), page: 1})
-            .subscribe((res) => {
-                should(res.statusCode).be.eql(200);
-                should(res).have.property('statusCode')
-                should(res).have.property('data')
-                should(res.data).have.property('data')
-                done()
-            })
-    });
+describe('Api service test', () => {
+    describe('Flights', () => {
+        it('should call api with success status code', (done) => {
+            getFlights({from: 'Paris', to: 'London', date: new Date('2018-02-13'), page: 1})
+                .subscribe((res) => {
+                    should(res.statusCode).be.eql(200);
+                    should(res).have.property('statusCode')
+                    should(res).have.property('data')
+                    should(res.data).have.property('data')
+                    done()
+                })
+        });
 
-    it('should call api with 500 status code', (done) => {
-        getFlights({from: 'Paris', to: 'London', date: new Date('xxx'), page: 1})
-            .subscribe((res) => {}, (err) => {
-                should(err.statusCode).be.eql(500);
-                should(err).have.property('statusCode')
-                should(err).have.property('errPayload')
-                done()
-            })
+        it('should call api with 500 status code', (done) => {
+            getFlights({from: 'Paris', to: 'London', date: new Date('xxx'), page: 1})
+                .subscribe((res) => {}, (err) => {
+                    should(err.statusCode).be.eql(500);
+                    should(err).have.property('statusCode')
+                    should(err).have.property('errPayload')
+                    done()
+                })
+        });
+    })
+    describe('Locations', () => {
 
-    });
+        it('should call api with 500 status code', (done) => {
+            getLocations('Prague')
+                .subscribe((res) => {
+                    res.should.have.property('statusCode');
+                    res.should.have.property('data');
+                    res.data.should.have.property('locations');
+                    done()
+                })
+        });
+    })
 })
--- src/api.service.ts
+++ src/api.service.ts
@@ -2,7 +2,7 @@ import {RxHR} from '@akanass/rx-http-request';
 import * as moment from 'moment';
 import {Observable} from 'rx';
 
-const FLIGHTS_URL = 'https://api.skypicker.com/flights';
+const BASE_URL = 'https://api.skypicker.com';
 const RESULTS_ON_PAGE = 5;
 
 interface AbstractResponse {
@@ -10,8 +10,8 @@ interface AbstractResponse {
     errPayload?: any;
 }
 
-interface HttpResponse<T> extends AbstractResponse{
-    data?: T;
+interface HttpResponse<T> extends AbstractResponse {
+    data: T;
 }
 
 interface GetFlights {
@@ -38,41 +38,68 @@ interface GetFlightsQueryParameters {
 interface GetFlightsResponse {
     [key: string]: any,
     data: {
-        mapIdfrom: string,
-        duration: Object,
-        flyTo: string,
-        conversion: Object,
-        mapIdto: string,
-        airlines: any[],
-        id: string,
-        facilitated_booking_available: boolean,
-        pnr_count: number,
-        fly_duration: string,
-        countryTo: Object,
-        baglimit: Object,
-        aTimeUTC: number,
-        p3: number,
-        price: number,
-        bags_price: Object,
-        cityTo: string,
-        flyFrom: string,
-        dTimeUTC: number,
-        p2: number,
-        countryFrom: Object,
-        p1: number,
-        dTime: number,
-        found_on: any[],
-        booking_token: string,
-        routes: any[],
-        cityFrom: string,
-        aTime: number,
-        route: any[],
-        distance: number
-
+        mapIdfrom: string;
+        duration: Object;
+        flyTo: string;
+        conversion: Object;
+        mapIdto: string;
+        airlines: any[];
+        id: string;
+        facilitated_booking_available: boolean;
+        pnr_count: number;
+        fly_duration: string;
+        countryTo: Object;
+        baglimit: Object;
+        aTimeUTC: number;
+        p3: number;
+        price: number;
+        bags_price: Object;
+        cityTo: string;
+        flyFrom: string;
+        dTimeUTC: number;
+        p2: number;
+        countryFrom: Object;
+        p1: number;
+        dTime: number;
+        found_on: any[];
+        booking_token: string;
+        routes: any[];
+        cityFrom: string;
+        aTime: number;
+        route: any[];
+        distance: number;
     }
 }
 
-export function getFlights(flightParameters: GetFlights): Observable<HttpResponse<GetFlightsResponse>> {
+interface GetLocationsResponse {
+    locations: {
+        id: string;
+        int_id: number;
+        active: boolean;
+        code: string;
+        name: string;
+        slug: string;
+        alternative_names: string[];
+        rank: number;
+        timezone: string;
+        city: {
+            id: string;
+            name: string;
+            code: string;
+            slug: string;
+            subdivision: any;
+            country: Object;
+            region: Object;
+            continent: Object;
+        };
+        location: {lat: number; lon: number};
+        alternative_departure_points: Object[];
+        type: string;
+    }[];
+    meta: any
+}
+
+export function getFlights(flightParameters: GetFlights) {
     const {page, date, from, to} = flightParameters;
     const queryParams: GetFlightsQueryParameters = {
         sort: 'price',
@@ -86,13 +113,18 @@ export function getFlights(flightParameters: GetFlights): Observable<HttpRespons
         dateFrom: moment(date).format('DD/MM/YYYY'),
         dateTo: moment(date).format('DD/MM/YYYY')
     }
-    return httpGet<GetFlightsResponse>(FLIGHTS_URL, queryParams);
+    return httpGet<GetFlightsResponse>('/flights', queryParams);
+}
+
+export function getLocations(term: string) {
+    const queryParams = {term, v: 2, locale: 'en-US'};
+    return httpGet<GetLocationsResponse>('/locations', queryParams);
 }
 
 function httpGet<T>(url: string, query: Object): Observable<HttpResponse<T>> {
     return Observable.fromPromise(new Promise((resolve, reject) => {
         RxHR
-            .get(url, {json: true, qs: query})
+            .get(BASE_URL + url, {json: true, qs: query})
             .subscribe((data) => {
                 if (data.response.statusCode < 400) {
                     resolve({statusCode: data.response.statusCode, data: data.response.body})
