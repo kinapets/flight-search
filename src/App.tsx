fd0444c1d61d64427edfc6db4a7ff2768ae35726
Radim Štěpaník <radim.stepanik@gmail.com>
2018-02-15 22:38:24 +0100

    chore(App): Remove not used things

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
 import * as React from 'react';
-import { DatePicker } from 'antd';
+import Form from './Form';
 import './App.css';
 import 'antd/dist/antd.css';
-
-const logo = require('./logo.svg');
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <DatePicker/>
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Form />
       </div>
     );
   }
